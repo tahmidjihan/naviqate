7717b3c15162d3ce3b2471069f53a92ddb4447b5
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-12 06:18:47 +0600

    added a special feature

--- src/Components/Destinations.jsx
+++ src/Components/Destinations.jsx
@@ -1,6 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Destinations() {
+  const modalRef = useRef(null);
+  const playerRef = useRef(null);
+  const ytPlayer = useRef(null);
+  const src =
+    'https://www.youtube.com/embed/dQw4w9WgXcQ?si=ghqlbh1cTxH8eNMV&amp;controls=0';
+  const showModal = () => {
+    document.getElementById('rickroll_modal').showModal();
+    loadYTScript();
+  };
+
+  const loadYTScript = () => {
+    if (!window.YT) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+    } else {
+      window.onYouTubeIframeAPIReady();
+    }
+
+    window.onYouTubeIframeAPIReady = () => {
+      ytPlayer.current = new window.YT.Player(playerRef.current, {
+        videoId: 'dQw4w9WgXcQ',
+        playerVars: {
+          autoplay: 1,
+          mute: 1,
+          controls: 0,
+        },
+        events: {
+          onReady: (e) => {
+            setTimeout(() => {
+              // Try to unmute after slight delay
+              ytPlayer.current.unMute();
+              ytPlayer.current.setVolume(100);
+            }, 0);
+          },
+        },
+      });
+    };
+  };
+  const closeModal = () => {
+    if (ytPlayer.current) {
+      ytPlayer.current.stopVideo();
+    }
+    document.getElementById('rickroll_modal').close();
+  };
   return (
     <div className='hero py-32'>
       <div className='hero-content flex-col lg:flex-row'>
@@ -37,18 +82,34 @@ function Destinations() {
             >
               <a href='#'>Dashboard {'>'}</a>
             </h3>
-            <h3
-              href='#'
-              className='text-xl text-cyan font-bold mb-7 w-full border-b-black border-b-2'
-            >
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+            <h3 className='text-xl text-cyan cursor-pointer font-bold mb-7 w-full border-b-black border-b-2'>
+              <span
+                onClick={showModal}
+                href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+              >
                 Special! {'>'}
-              </a>
+              </span>
             </h3>
           </div>
           <button className='btn primary-btn'>Get started</button>
         </div>
       </div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <dialog id='rickroll_modal' ref={modalRef} className='modal w-full'>
+        <div className='modal-box px-4 py-8 text-center '>
+          <h3 className='font-bold text-lg'>YOU’VE BEEN RICKED 🤡</h3>
+          <div className='py-4'>
+            <div className='max-w-md'>
+              <div id='yt-container'>
+                <div ref={playerRef} className='max-w-md' />
+              </div>
+            </div>
+          </div>
+          <form method='dialog' onSubmit={closeModal}>
+            <button className='btn'>Close</button>
+          </form>
+        </div>
+      </dialog>
     </div>
   );
 }
