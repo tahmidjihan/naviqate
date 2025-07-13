12ced9dbc2ee08b31ef12e93438e4134a647d5d1
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-13 09:59:27 +0600

    added Charts

---
+++ src/Dashboard/Components/DashboardTable.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+
+function DashboardTable() {
+  return (
+    <div className='card rounded-3xl bg-base-100 w-full cyan-shadow'>
+      <div className='card-body'>
+        <h2 className='card-title font-bold text-2xl'>Databases</h2>
+        <div>
+          <div className='overflow-x-auto'>
+            <table className='dashboard-table w-full font-semibold text-md'>
+              <thead>
+                <tr>
+                  <td colSpan={4} className='py-2'>
+                    <div
+                      className={`flex items-center justify-between px-6 py-3`}
+                    >
+                      <div className='w-1/12'>#</div>
+                      <div className='w-3/12'>Name</div>
+                      <div className='w-3/12'>Data</div>
+                      <div className='w-2/12'>Undone</div>
+                      <div className='w-3/12'>Last updated</div>
+                      <div className='w-3/12'>Status</div>
+                      <div className='w-5/12'></div>
+                    </div>
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                {[
+                  {
+                    name: 'Sales',
+                    data: 1345,
+                    undone: 7,
+                    lastUpdated: '2025. 07.05',
+                    status: 'active',
+                  },
+                  {
+                    name: 'Queries',
+                    data: 2434,
+                    undone: 10,
+                    lastUpdated: '2025. 07.02',
+                    status: 'active',
+                  },
+                  {
+                    name: 'sales II',
+                    data: 2434,
+                    undone: 10,
+                    lastUpdated: '2025. 03.25',
+                    status: 'Paused',
+                  },
+                ].map((item, i) => {
+                  return (
+                    <tr key={i}>
+                      <td colSpan={4} className='py-2'>
+                        <div
+                          className={`flex items-center justify-between px-6 py-3 bg-gray-100 rounded-full shadow-sm`}
+                        >
+                          <div className='w-1/12'>{i + 1}</div>
+                          <div className='w-3/12'>{item.name}</div>
+                          <div className='w-3/12'>{item.data}</div>
+                          <div className='w-2/12'>{item.undone}</div>
+                          <div className='w-3/12'>{item.lastUpdated}</div>
+                          <div
+                            className={`w-3/12 px-3 py-1 bg-${
+                              item.status !== 'active' ? 'red' : 'cyan'
+                            }-200 rounded-lg`}
+                          >
+                            {item.status}
+                          </div>
+                          <div className='w-5/12 text-cyan font-bold text-right'>
+                            See more
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardTable;
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,90 +1,53 @@
 import React from 'react';
+import DashboardTable from './Components/DashboardTable';
+import {
+  Bar,
+  ResponsiveContainer,
+  Tooltip,
+  BarChart,
+  Legend,
+  XAxis,
+} from 'recharts';
 
 function Dashboard() {
+  const data = [
+    { name: 'Jan', users: 1346 },
+    { name: 'Feb', users: 987 },
+    { name: 'Mar', users: 1523 },
+    { name: 'Apr', users: 1104 },
+    { name: 'May', users: 1327 },
+    { name: 'Jun', users: 1452 },
+    { name: 'Jul', users: 1398 },
+    { name: 'Aug', users: 689 },
+    { name: 'Sep', users: 867 },
+    { name: 'Oct', users: 1003 },
+    { name: 'Nov', users: 932 },
+    { name: 'Dec', users: 1499 },
+  ];
   return (
     <div className='bg-white min-h-screen w-full'>
       <div className='p-10'>
         <h2 className='text-2xl'>Dashboard</h2>
         <div className='py-2'>
-          <div className='card rounded-3xl bg-base-100 w-full cyan-shadow'>
-            <div className='card-body'>
-              <h2 className='card-title font-bold text-2xl'>Databases</h2>
-              <div>
-                <div className='overflow-x-auto'>
-                  <table className='dashboard-table w-full font-semibold text-md'>
-                    <thead>
-                      <tr>
-                        <td colSpan={4} className='py-2'>
-                          <div
-                            className={`flex items-center justify-between px-6 py-3`}
-                          >
-                            <div className='w-1/12'>#</div>
-                            <div className='w-3/12'>Name</div>
-                            <div className='w-3/12'>Data</div>
-                            <div className='w-2/12'>Undone</div>
-                            <div className='w-3/12'>Last updated</div>
-                            <div className='w-3/12'>Status</div>
-                            <div className='w-5/12'></div>
-                          </div>
-                        </td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {[
-                        {
-                          name: 'Sales',
-                          data: 1345,
-                          undone: 7,
-                          lastUpdated: '2025. 07.05',
-                          status: 'active',
-                        },
-                        {
-                          name: 'Queries',
-                          data: 2434,
-                          undone: 10,
-                          lastUpdated: '2025. 07.02',
-                          status: 'active',
-                        },
-                        {
-                          name: 'sales II',
-                          data: 2434,
-                          undone: 10,
-                          lastUpdated: '2025. 03.25',
-                          status: 'Paused',
-                        },
-                      ].map((item, i) => {
-                        return (
-                          <tr key={i}>
-                            <td colSpan={4} className='py-2'>
-                              <div
-                                className={`flex items-center justify-between px-6 py-3 bg-gray-100 rounded-full shadow-sm`}
-                              >
-                                <div className='w-1/12'>{i + 1}</div>
-                                <div className='w-3/12'>{item.name}</div>
-                                <div className='w-3/12'>{item.data}</div>
-                                <div className='w-2/12'>{item.undone}</div>
-                                <div className='w-3/12'>{item.lastUpdated}</div>
-                                <div
-                                  className={`w-3/12 px-3 py-1 bg-${
-                                    item.status !== 'active' ? 'red' : 'cyan'
-                                  }-200 rounded-lg`}
-                                >
-                                  {item.status}
-                                </div>
-                                <div className='w-5/12 text-cyan font-bold text-right'>
-                                  See more
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </table>
+          <div>
+            <div>
+              <div className='card rounded-3xl cyan-shadow mb-5 bg-base-100 w-[600px]'>
+                <div className='card-body'>
+                  <h2 className='font-bold text-3xt text-gray-600'>
+                    Visits On Last Year
+                  </h2>
+                  <ResponsiveContainer width='100%' height={300}>
+                    <BarChart data={data}>
+                      <Bar dataKey='users' fill='#00bbc1' />
+                      <XAxis dataKey='name' />
+                      <Tooltip />
+                    </BarChart>
+                  </ResponsiveContainer>
                 </div>
               </div>
             </div>
           </div>
+          <DashboardTable />
         </div>
       </div>
     </div>
