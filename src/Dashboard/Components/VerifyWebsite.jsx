dcd5684ae7542bf971953d0c840d756745da221b
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-31 15:15:39 +0600

    made the verify website working

--- src/Dashboard/Components/VerifyWebsite.jsx
+++ src/Dashboard/Components/VerifyWebsite.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../AuthProvider';
 import { useQuery } from '@tanstack/react-query';
+import { Link } from 'react-router';
 
 function VerifyWebsite() {
   const [website, setWebsite] = useState(false);
@@ -22,7 +23,26 @@ function VerifyWebsite() {
   useEffect(() => {
     refetch();
   });
-  return <div>VerifyWebsite</div>;
+  return (
+    <div className='w-screen bg-[rgba(0,0,0,0.5)] h-screen absolute z-[5] flex items-center justify-center'>
+      <div className='card bg-white rounded-2xl shadow-lg cyan-shadow'>
+        <div className='card-body max-w-md'>
+          <h2 className='card-title text-3xl'>Please Verify your website</h2>
+          <p>
+            Contact us to place order of your new website on chats on contact
+            page and if you have a website please go to contact page and inform
+            to us. If you have further queries please contact us in contact
+            page.
+          </p>
+          <div className='card-actions'>
+            <Link to='/dashboard/contact' className='btn primary-btn'>
+              Contact Page
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VerifyWebsite;
--- src/Dashboard/WebManager.jsx
+++ src/Dashboard/WebManager.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaCircleInfo, FaFileArrowDown } from 'react-icons/fa6';
+import VerifyWebsite from './Components/VerifyWebsite';
 import {
   Area,
   AreaChart,
@@ -21,6 +22,7 @@ function WebManager() {
   ];
   return (
     <div className='bg-white min-h-screen w-full'>
+      <VerifyWebsite></VerifyWebsite>
       <div className='p-4 my-10 md:p-10 overflow-x-hidden container'>
         <h2 className='text-xl md:text-2xl'>Web Manager</h2>
         <div className='py-2 flex flex-col lg:grid grid-cols-1 lg:grid-cols-3 gap-5'>
