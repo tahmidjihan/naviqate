53eab825381ba98f0c429ad83ded0af0741b3743
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-31 15:35:29 +0600

    made the table of dbTables-data

--- src/Dashboard/Components/DashboardTable.jsx
+++ src/Dashboard/Components/DashboardTable.jsx
@@ -66,7 +66,44 @@ function DashboardTable({ count, lastUpdate }) {
               </button>
             </form>
             <h3 className='font-bold text-lg'>Add data !</h3>
-            <p className='py-4'>This is the data of {id}</p>
+            <div>
+              <div className='overflow-x-auto s-table rounded-box border border-base-content/5 bg-base-100'>
+                <table className='table'>
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>Name</th>
+                      <th>Job</th>
+                      <th>Favorite Color</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* row 1 */}
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                    </tr>
+                    {/* row 2 */}
+                    <tr>
+                      <th>2</th>
+                      <td>Hart Hagerty</td>
+                      <td>Desktop Support Technician</td>
+                      <td>Purple</td>
+                    </tr>
+                    {/* row 3 */}
+                    <tr>
+                      <th>3</th>
+                      <td>Brice Swyre</td>
+                      <td>Tax Accountant</td>
+                      <td>Red</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
             <p className='py-4'>Press ESC key or click on ✕ button to close</p>
           </div>
         </dialog>
