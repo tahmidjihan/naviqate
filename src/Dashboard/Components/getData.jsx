250de41490a2aec86aa18ba9e453a9405377941a
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-08-01 22:42:55 +0600

    done making get data mostly

--- src/Dashboard/Components/getData.jsx
+++ src/Dashboard/Components/getData.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { FaCircleInfo } from 'react-icons/fa6';
 
 function GetData({ id }) {
   const [selectedId, setSelectedId] = useState(null);
+  const [data, setData] = useState(null);
   const handleClick = () => {
     const modal = document.getElementById(`data_modal_${id}`);
     setSelectedId(id);
@@ -12,6 +15,16 @@ function GetData({ id }) {
     setSelectedId(null);
     modal.close();
   };
+  useEffect(() => {
+    if (selectedId) {
+      axios
+        .get(`${import.meta.env.VITE_BACKEND}/getData?db_id=${selectedId}`)
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        });
+    }
+  }, [selectedId]);
   return (
     <>
       <span
@@ -24,47 +37,49 @@ function GetData({ id }) {
         <div className='modal-box'>
           <div>
             {/* if there is a button in form, it will close the modal */}
-            <button className='btn btn-sm btn-circle btn-ghost text-2xl absolute right-2 top-2'>
+            <button
+              onClick={() => handleClose()}
+              className='btn btn-sm btn-circle btn-ghost text-2xl absolute right-2 top-2'
+            >
               ✕
             </button>
           </div>
-          <h3 className='font-bold text-lg text-black'>Add data !</h3>
+          <h3 className='font-bold text-lg text-black'>
+            Get data of database !
+          </h3>
           <div>
             <div className='overflow-x-auto s-table rounded-box border border-base-content/5 bg-base-100'>
-              <table className='table'>
-                {/* head */}
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>Name</th>
-                    <th>Job</th>
-                    <th>Favorite Color</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* row 1 */}
-                  <tr>
-                    <th>1</th>
-                    <td>Cy Ganderton</td>
-                    <td>Quality Control Specialist</td>
-                    <td>Blue</td>
-                  </tr>
-                  {/* row 2 */}
-                  <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
-                    <td>Purple</td>
-                  </tr>
-                  {/* row 3 */}
-                  <tr>
-                    <th>3</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
-                  </tr>
-                </tbody>
-              </table>
+              {data ? (
+                <table className='table'>
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      {Object.keys(data[0].data).map((key) => (
+                        <th key={key}>{key}</th>
+                      ))}
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* row 1 */}
+                    {data.map((item, index) => (
+                      <tr key={index}>
+                        {Object.values(item.data).map((value, index) => (
+                          <td key={index}>{value}</td>
+                        ))}
+                        <td>{item.undone == true ? 'Pending' : 'Done'}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : (
+                <>
+                  <div className='badge badge-info'>
+                    <FaCircleInfo />
+                    <span className='ml-2'>No data found</span>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
